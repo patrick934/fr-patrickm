92b071e6e501b548d11f3d88631f7d75cae4aaea
Patrick Mullen <prmullen20@gmail.com>
2020-10-26 11:52:56 -0700

    save

--- client/index.js
+++ client/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './components/app';
+import App from './components/App';
 
 
-
-
-// should grab the App component and attach to the index.html
 render(<App />,
   document.getElementById('root'),
 );
